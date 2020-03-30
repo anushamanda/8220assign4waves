3748c5358edec917a66bb2535b4db44ac81a1272
unknown <amanda@unomaha.edu>
2020-03-29 20:47:33 -0500

    first commit

---
+++ supervisor/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Supervisor(models.Model):
+
+    name=models.CharField(max_length=100)
+    employee_id=models.CharField(max_length=100)
+    phone=models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    role=models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
